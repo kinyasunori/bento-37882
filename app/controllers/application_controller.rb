b8037df6fabaad4c88464eb8bc7646c1cb3d63c8
kinyasunori <oneforalltk0826@gmail.com>
2022-06-11 21:23:08 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'kysnr' && password == '0826'
+    end
+  end
 end
