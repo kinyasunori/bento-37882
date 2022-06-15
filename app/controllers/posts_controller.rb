5665bcca4bccf5cc26e0a75bca399928ccec236b
kinyasunori <oneforalltk0826@gmail.com>
2022-06-15 16:45:20 +0900

    投稿機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,15 +3,37 @@ class PostsController < ApplicationController
   before_action :move_to_index, except: [:index, :new, :create, :show]
 
   def index
+    @posts = Post.all.includes(:user)
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path(@post)
+    else
+      render :new
+      @post = Post.includes(:user)
+    end
+  end
+
+  
 
   
 
   private
 
   def move_to_index
-    unless user_signed_in?
+    @post = Post.find(params[:id])
+    unless user_signed_in? && current_user.id == @post.user_id
       redirect_to action: :index
     end
   end
+
+  def post_params
+    params.require(:post).permit(:title, :menu, :recipe, :image).merge(user_id: current_user.id)
+  end 
 end
