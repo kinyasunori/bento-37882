87eff2d23dc60d2ded6d6b15df7a083613bee342
kinyasunori <oneforalltk0826@gmail.com>
2022-06-16 15:38:43 +0900

    Revert "投稿編集機能"
    This reverts commit 97f80d51e31086d9fdced361144a587c6af96cc9.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,19 +24,6 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
-  def edit
-    @post = Post.find(params[:id])
-  end
-
-  def update
-    @post = Post.find(params[:id])
-    if @post.update(post_params)
-      redirect_to post_path
-    else
-      render :edit
-    end
-  end
-
   
 
   
